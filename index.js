e90b745e7b6da09fc4ce2a769536b48064aae0ec
LainaJ <lainakarosic@gmail.com>
2019-11-23 11:39:29 -0500

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
 // Your code goes here
+
+document.addEventListener("DOMContentLoaded", function(){
+    giveText()
+})
+
+//find the p tag 
+//change it to the text below
+function giveText() {
+   let para = document.getElementById("text")
+   para.innerHTML = "This is really cool!"
+}
